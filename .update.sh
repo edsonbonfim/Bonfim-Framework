0ba55f911153d86fcd7be2821e330003f3dc1364
Edson Onildo <edsononildo@id.uff.br>
2018-11-05 14:53:27 -0200

    Config git subsplit

---
+++ .update.sh
@@ -0,0 +1,5 @@
+git subsplit publish "
+    src/SketchPHP/View:git@github.com:SketchPHP/View.git
+    src/SketchPHP/Database:git@github.com:SketchPHP/Database.git
+    src/SketchPHP/Http:git@github.com:SketchPHP/Http.git
+" --update --heads=master
